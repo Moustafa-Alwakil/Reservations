3513c25a75016d10609121e57fce8a98fb41ca88
Moustafa-Alwakil <moustafaalwakil@gmail.com>
2021-09-13 02:36:26 +0200

    Eloquent Filter Package Has Been Removed

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class,
-        EloquentFilter\ServiceProvider::class,
 
         /*
          * Package Service Providers...
